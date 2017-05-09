2fb1a26fff47b3bb2e1332f5632ea89f251fbd13
Andrey_Chadov <androusj@mail.ru>
2017-05-09 12:15:26 +0600

    [IMP] Implemented OneTOMany in stedunt class by exams field

--- src/main/java/com/hillel/webinar/hibernate/entity/Student.java
+++ src/main/java/com/hillel/webinar/hibernate/entity/Student.java
@@ -4,6 +4,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.util.Set;
 
 /**
  *
@@ -31,6 +32,11 @@ public class Student extends AbstractEntity {
     @Setter
     @Embedded
     private StudentAddress studentAddress;
+
+    @Getter
+    @Setter
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "student")
+    private Set<Exam> exams;
 
     @Override
     public boolean equals(Object o) {
